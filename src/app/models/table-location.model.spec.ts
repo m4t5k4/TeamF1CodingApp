4a6e96e27df0551bd10d484fc51c7fc16937b0b3
Walid Amhaini <r0740199@student.thomasmore.be>
2021-01-19 09:47:21 +0100

    model aangepast

---
+++ src/app/models/table-location.model.spec.ts
@@ -0,0 +1,7 @@
+import { TableLocation } from './table-location.model';
+
+describe('TableLocation', () => {
+  it('should create an instance', () => {
+    expect(new TableLocation()).toBeTruthy();
+  });
+});
--- src/app/models/table-location.model.ts
+++ src/app/models/table-location.model.ts
@@ -1,4 +1,4 @@
-export class Table {
+export class TableLocation {
     constructor(
         public id: number,
         public name: string,
