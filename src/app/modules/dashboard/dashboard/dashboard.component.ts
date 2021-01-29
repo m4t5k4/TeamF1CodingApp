a9800a06c966485dfc1d8cbdb05c5a533c4fbbea
Ruben Vanderborght <r0749158@student.thomasmore.be>
2021-01-29 11:46:41 +0100

    refresh aantal mensen binnen & layout home page

--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { DashboardService} from '../dashboard.service';
-import { Iot} from '../../../shared/models/iot.model';
-import {formatDate} from '@angular/common';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { DashboardService } from '../dashboard.service';
+import { Iot } from '../../../shared/models/iot.model';
+import { formatDate } from '@angular/common';
 import { registerLocaleData } from '@angular/common';
 import localeNlbe from '@angular/common/locales/nl-BE';
 import localeNlBeExtra from '@angular/common/locales/extra/nl-BE';
 
-import {interval} from 'rxjs';
+import { interval } from 'rxjs';
 
-registerLocaleData(localeNlbe,localeNlBeExtra);
+registerLocaleData(localeNlbe, localeNlBeExtra);
 
 @Component({
   selector: 'app-dashboard',
@@ -16,24 +16,49 @@ registerLocaleData(localeNlbe,localeNlBeExtra);
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
+  @ViewChild('hrHand', { static: false }) hrHand: ElementRef;
+  @ViewChild('minHand', { static: false }) minHand: ElementRef;
+  @ViewChild('secHand', { static: false }) secHand: ElementRef;
+
   iots: Iot[];
-  public iot_output = new Iot(0,4, new Date());
+  public iot_output = new Iot(0, 4, new Date());
   public last_update = "";
   public date = new Date;
 
-  constructor(private _dashboardService : DashboardService) { 
+  constructor(private _dashboardService: DashboardService) {
     this._dashboardService.getIot().subscribe(
-      result =>
-      {
+      result => {
         this.iots = result;
-        this.iot_output = this.iots[this.iots.length -1];
+        this.iot_output = this.iots[this.iots.length - 1];
         this.date = new Date(this.iot_output.timeStamp);
-        this.last_update = formatDate(this.date.setHours(this.date.getHours() - 1), 'dd/MM/yyyy HH:mm','en-UK');
+        this.last_update = formatDate(this.date.setHours(this.date.getHours() - 1), 'dd/MM/yyyy HH:mm', 'en-UK');
       }
     );
   }
 
   ngOnInit(): void {
+    setInterval(() => {
+      const date = new Date();
+      this.updateClock(date);
+      this._dashboardService.getIot().subscribe(
+        result => {
+          this.iots = result;
+          this.iot_output = this.iots[this.iots.length - 1];
+          this.date = new Date(this.iot_output.timeStamp);
+          this.last_update = formatDate(this.date.setHours(this.date.getHours() - 1), 'dd/MM/yyyy HH:mm', 'en-UK');
+        }
+      );
+    }, 1000)
+  }
+
+  updateClock(date) {
+    this.secHand.nativeElement.style.transform = 'rotate(' +
+      date.getSeconds() * 6 + 'deg)';
+    this.minHand.nativeElement.style.transform = 'rotate(' +
+      date.getMinutes() * 6 + 'deg)';
+    this.hrHand.nativeElement.style.transform = 'rotate(' +
+      (date.getHours() * 30 + date.getMinutes() * 0.5) + 'deg)';
   }
 
   refresh(): void {
