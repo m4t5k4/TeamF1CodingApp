56c94c2eec57c09d4820c13207f516039d167c63
Ruben Vanderborght <r0749158@student.thomasmore.be>
2021-02-03 11:07:36 +0100

    sonarcube

--- src/app/shared/models/place.model.spec.ts
+++ src/app/shared/models/place.model.spec.ts
@@ -1,7 +1,24 @@
 import { Place } from './place.model';
+import { Location } from './location.model';
+import { TableLocation } from './table-location.model';
+
+function getRandomString(length) {
+  var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+  for ( var i = 0; i < length; i++ ) {
+      result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result;
+}
+
+var Location1 = new Location(Math.floor(Math.random() * 10), getRandomString(10), getRandomString(10), getRandomString(10));
+var TableLocation1 = new TableLocation(Math.floor(Math.random() * 10), getRandomString(10), getRandomString(10), Location1);
+
 
 describe('Place', () => {
   it('should create an instance', () => {
-    expect(new Place()).toBeTruthy();
+    expect(new Place(Math.floor(Math.random() * 10),"Name", TableLocation1,false)).toBeTruthy();
   });
 });
+
+
--- src/app/shared/models/scenario.model.spec.ts
+++ src/app/shared/models/scenario.model.spec.ts
@@ -1,7 +1,16 @@
 import { Scenario } from './scenario.model';
+function getRandomString(length) {
+  var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+  for ( var i = 0; i < length; i++ ) {
+      result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result;
+}
+
 
 describe('Scenario', () => {
   it('should create an instance', () => {
-    expect(new Scenario()).toBeTruthy();
+    expect(new Scenario(Math.floor(Math.random() * 10),getRandomString(10))).toBeTruthy();
   });
 });
